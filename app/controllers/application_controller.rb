78b398943de692fe15e992981b2e336a65687aa8
Kayo Pereira <kayo.richard2000@gmail.com>
2023-04-03 23:30:03 -0300

    adicionado um javascript para sumir com o flash notice e um style a mais no notice padrão

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,20 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :role)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :role) }
 
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :current_password, :role)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :current_password, :role) }
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "Você não está autorizado a executar esta ação."
+    redirect_to(request.referrer || root_path)
   end
 end
